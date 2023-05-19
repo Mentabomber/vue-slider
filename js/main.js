ef63a520c3df49572466ac6fceace720a159f004
Menta <simcictilen.gmail.com>
2023-05-19 16:14:17 +0200

    thumb-nail active

--- js/main.js
+++ js/main.js
@@ -14,13 +14,25 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
+            indexImmagineAttiva : 0,
             images: ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"],
             title: ["Marvel's Spiderman Miles Morale", 'Ratchet & Clank: Rift Apart', 'Fortnite', 'Stray', "Marvel's Avengers",],
             text: ['Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', 'Go dimension-hopping with        Ratchet and Clank as they take on an evil emperor from another reality.', "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.", 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city', "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",]
         }
     },
     methods:{
-
+        arrowUp (){
+            this.indexImmagineAttiva--;
+            if(this.indexImmagineAttiva < 0){
+                this.indexImmagineAttiva = this.images.length - 1;
+            }
+        },
+        arrowDown (){
+            this.indexImmagineAttiva++;
+            if(this.indexImmagineAttiva === this.images.length){
+                this.indexImmagineAttiva = 0;
+            }
+        }
     },
     mounted(){
 
@@ -30,171 +42,126 @@ createApp({
 
 
 
-let indexImmagineAttiva = 0;
-
-let containerSelector = document.getElementById("container");
-
-
-images.forEach((immagine) => {
-
-// creazione div ed immagini dentro id = container
-
-    containerSelector = document.getElementById("container");
-
-    containerSelector.innerHTML += `<div class="img-container"> 
-                                        <img src="${immagine.image}" alt="static-test-img">
-                                        <div class="title-text">
-                                            <h2>
-                                            ${immagine.title}
-                                            </h2>
-                                            <p>
-                                            ${immagine.text}
-                                            </p>
-                                        </div>
-                                    </div>
-                             
-                                    `;
-    
-    // creazione div ed immagini dentro id = thumb-nail
-
-    containerSelector = document.getElementById("thumb-nail");
-
-
-    containerSelector.innerHTML += `<div class="img-thumb-nail"> 
-                                        <img src=${immagine.image} alt="static-thumb-nail-test-img">
-                                    </div>
-                                    ` ;
-                                    
-
-
-});
 
 
 
-// seleziono container poi seleziono tutti i div con classe img-container e aggiungo alla prima active per visualizzare il div 
 
-let container = document.getElementById("container");
-let immagini = container.querySelectorAll("div.img-container");
-immagini[indexImmagineAttiva].classList.add("active");
 
-// seleziono tutti i div con classe img-thumb-nail e aggiungo alla prima active per visualizzare il div 
 
-let immaginiThumbNail = container.querySelectorAll("div.img-thumb-nail");
-immaginiThumbNail[indexImmagineAttiva].classList.add("thumb-nail-selected")
 
 // bottone down per muovermi da un'immagine verso l'altra dall'alto verso il basso
 
-let  buttonInteractionUp = document.getElementById("arrow-down-carousel");
-buttonInteractionUp.addEventListener('click', arrowUp);
+// let  buttonInteractionUp = document.getElementById("arrow-down-carousel");
+// buttonInteractionUp.addEventListener('click', arrowUp);
 
-function arrowUp (){
+// function arrowUp (){
 
-        immagini[indexImmagineAttiva].classList.remove("active");
+//         immagini[indexImmagineAttiva].classList.remove("active");
 
-        immaginiThumbNail[indexImmagineAttiva].classList.remove("thumb-nail-selected");
-        indexImmagineAttiva++;
+//         immaginiThumbNail[indexImmagineAttiva].classList.remove("thumb-nail-selected");
+//         indexImmagineAttiva++;
 
-        if(indexImmagineAttiva == imageList.length ){
-            indexImmagineAttiva = 0;
+//         if(indexImmagineAttiva == imageList.length ){
+//             indexImmagineAttiva = 0;
 
-        }
-        immagini[indexImmagineAttiva].classList.add("active");
+//         }
+//         immagini[indexImmagineAttiva].classList.add("active");
 
-        immaginiThumbNail[indexImmagineAttiva].classList.add("thumb-nail-selected");
+//         immaginiThumbNail[indexImmagineAttiva].classList.add("thumb-nail-selected");
 
-        console.log(indexImmagineAttiva);
-};
+//         console.log(indexImmagineAttiva);
+// };
 
-// bottone down per muovermi da un'immagine verso l'altra dal basso verso l'alto
+// // bottone down per muovermi da un'immagine verso l'altra dal basso verso l'alto
 
-let  buttonInteractionDown = document.getElementById("arrow-up-carousel");
+// let  buttonInteractionDown = document.getElementById("arrow-up-carousel");
 
-buttonInteractionDown.addEventListener('click', arrowDown);
+// buttonInteractionDown.addEventListener('click', arrowDown);
     
 
 
-function arrowDown(){
+// function arrowDown(){
 
-        immagini[indexImmagineAttiva].classList.remove("active");
+//         immagini[indexImmagineAttiva].classList.remove("active");
 
-        immaginiThumbNail[indexImmagineAttiva].classList.remove("thumb-nail-selected");
+//         immaginiThumbNail[indexImmagineAttiva].classList.remove("thumb-nail-selected");
 
-        if(indexImmagineAttiva == 0 ){
-            indexImmagineAttiva = imageList.length;
+//         if(indexImmagineAttiva == 0 ){
+//             indexImmagineAttiva = imageList.length;
 
-        }
+//         }
 
-        indexImmagineAttiva--;
+//         indexImmagineAttiva--;
 
-        immagini[indexImmagineAttiva].classList.add("active");
+//         immagini[indexImmagineAttiva].classList.add("active");
 
-        immaginiThumbNail[indexImmagineAttiva].classList.add("thumb-nail-selected");
+//         immaginiThumbNail[indexImmagineAttiva].classList.add("thumb-nail-selected");
         
-        console.log(indexImmagineAttiva);
-};
+//         console.log(indexImmagineAttiva);
+// };
 
-// Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
+// // Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
 
 
-function autoPlay(){
-return setInterval(arrowDown, 3000);
-}
+// function autoPlay(){
+// return setInterval(arrowDown, 3000);
+// }
 
-let counter = 2;
+// let counter = 2;
 
-function autoPlayReverse(){
-    return setInterval(function(){
+// function autoPlayReverse(){
+//     return setInterval(function(){
         
-        console.log(counter + "counter");
-        if (counter % 2 == 0){
+//         console.log(counter + "counter");
+//         if (counter % 2 == 0){
 
-            arrowDown();
+//             arrowDown();
             
-        }
-        else if(counter % 2 == 1){
+//         }
+//         else if(counter % 2 == 1){
 
-           arrowUp();
+//            arrowUp();
      
-        }  
-    }
-    , 3000);
-    }
+//         }  
+//     }
+//     , 3000);
+//     }
 
-let clock = autoPlay();
+// let clock = autoPlay();
 
-// stop button
+// // stop button
 
-let buttonInteractionStop = document.getElementById("stop-button");
+// let buttonInteractionStop = document.getElementById("stop-button");
 
-buttonInteractionStop.addEventListener('click', () => {
-    clearInterval(clock);
-    buttonInteractionStop.className = "hidden";
-    buttonInteractionStart.className = "active";
-}
-
-
-);
-
-//start button
-
-let buttonInteractionStart = document.getElementById("start-button");
-
-buttonInteractionStart.addEventListener('click', () => {
-    clock = autoPlay();
-    buttonInteractionStop.className = "active";
-    buttonInteractionStart.className = "hidden";
-});
+// buttonInteractionStop.addEventListener('click', () => {
+//     clearInterval(clock);
+//     buttonInteractionStop.className = "hidden";
+//     buttonInteractionStart.className = "active";
+// }
 
 
-//reverse button
+// );
 
-let buttonInteractionReverse = document.getElementById("reverse-button");
+// //start button
 
-buttonInteractionReverse.addEventListener('click', () => {
-    counter++;
-    clearInterval(clock);
-    clock = autoPlayReverse();
-    buttonInteractionStop.className = "active";
-    buttonInteractionStart.className = "hidden";
+// let buttonInteractionStart = document.getElementById("start-button");
 
-});
+// buttonInteractionStart.addEventListener('click', () => {
+//     clock = autoPlay();
+//     buttonInteractionStop.className = "active";
+//     buttonInteractionStart.className = "hidden";
+// });
+
+
+// //reverse button
+
+// let buttonInteractionReverse = document.getElementById("reverse-button");
+
+// buttonInteractionReverse.addEventListener('click', () => {
+//     counter++;
+//     clearInterval(clock);
+//     clock = autoPlayReverse();
+//     buttonInteractionStop.className = "active";
+//     buttonInteractionStart.className = "hidden";
+
+// });
