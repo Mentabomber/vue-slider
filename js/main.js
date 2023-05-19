4cac3874d529990416b285b492114b789107bb98
Menta <simcictilen.gmail.com>
2023-05-19 17:17:48 +0200

    fix html v-for + prove stop auto play

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
+            interval: undefined,
             counter : 2,
             indexImmagineAttiva : 0,
             images: ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"],
@@ -53,10 +54,16 @@ createApp({
                 }  
             }
             , 3000);
-            }
+        },
+        stopAutoPlay(){
+            clearInterval(this.interval);
+            this.inteval = undefined;
+        }
     },
     mounted(){
+        // this.interval = 
         this.autoPlay();
+
     }
 
 }).mount('#app')
